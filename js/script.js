7c29be3db60b68105f8b170fd228e5905669d76b
sittikhairunnisak <132251307+sittikhairunnisak@users.noreply.github.com>
2023-12-05 10:25:11 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarnav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarnav.classList.toggle("active");
+};
+
+// Klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarnav.classList.remove("active");
+  }
+});
